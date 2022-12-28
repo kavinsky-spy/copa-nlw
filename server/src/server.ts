04cb96d62634a60f3a6169b19f3e82971bbaa9de
julio <Julio@zoocha.com>
2022-12-28 13:23:06 -0300

    add route to create pools

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import { Prisma, PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -23,8 +25,24 @@ async function bootstrap() {
 
 
     fastify.post('/pools', async (request, reply) => {
-        const { title } = request.body
 
+        const createPoolBody = z.object({
+            title: z.string(),
+
+        })
+
+        const { title } = createPoolBody.parse(request.body)
+
+        const generate = new ShortUniqueId({ length: 6 })
+        const code = String(generate()).toUpperCase()
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+        return reply.status(201).send({ code })
     })
 
     await fastify.listen({ port: 3333, host: '0.0.0.0' })
